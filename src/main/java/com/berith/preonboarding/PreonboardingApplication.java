5770c6429a51db1d9b15785e46216b7ab11c52c9
Berithx <seere7777@gmail.com>
2024-09-03 14:28:05 +0900

    init: 프로젝트 설정

---
+++ src/main/java/com/berith/preonboarding/PreonboardingApplication.java
@@ -0,0 +1,14 @@
+package com.berith.preonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class PreonboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreonboardingApplication.class, args);
+    }
+
+}
